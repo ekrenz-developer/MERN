e82f3e1482235d515dd554a4dc5250e8357a6042
Erik Krenz <ekrenz@MacBook-Pro-de-Erik.local>
2019-11-04 17:37:35 -0300

    Se agrega parte del back

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-console.log('Server is up!') 
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
+app.listen(8080, () => {
+    console.log('Server is up!') 
+})
